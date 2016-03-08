a69dd9c5e839a84e4597a913a7d9c8dfcde26291
James Crockett <jecrockett@gmail.com>
2016-03-08 09:32:29 -0700

    Returning all items, trying to remove timestamps with serializer

--- spec/controllers/api_items_controller_spec.rb
+++ spec/controllers/api_items_controller_spec.rb
@@ -2,9 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::ItemsController, type: :controller do
   describe '#index' do
-    get :index
+    it "returns all items" do
+      get :index, format: :json
 
-    expect(response).to be_success
-    binding.pry
+      expect(response).to be_success
+    end
   end
 end
