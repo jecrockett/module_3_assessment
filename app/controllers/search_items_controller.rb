54ffc62e5f7a6dd00af64c1e07667eba838f8b6e
James Crockett <jecrockett@gmail.com>
2016-03-13 11:48:17 -0600

    Refactored service a little. Getting successful response now.

--- app/controllers/search_items_controller.rb
+++ app/controllers/search_items_controller.rb
@@ -1,12 +1,11 @@
 class SearchItemsController < ApplicationController
 
   def index
-    binding.pry
-    best_buy = BestBuyService.new
-    search_terms = "(longDescription=#{params[:item]})"
-    @items = best_buy.search(search_terms)
+    best_buy = BestBuyService.new(params[:search])
+    response = best_buy.search
+    @items = response["products"]
   end
 
 end
 
-https://api.bestbuy.com/v1/products(longDescription=sennheiser)?show=sku,name,customerReviewAverage,shortDescription,salePrice,image&pageSize=15&apiKey=qp5arx9ga4bna25z688na79h&format=json
+# https://api.bestbuy.com/v1/products(longDescription=sennheiser)?show=sku,name,customerReviewAverage,shortDescription,salePrice,image&pageSize=15&apiKey=qp5arx9ga4bna25z688na79h&format=json
--- app/models/best_buy_service.rb
+++ app/models/best_buy_service.rb
@@ -1,23 +1,21 @@
 class BestBuyService
   attr_reader :conn
 
-  def initialize
-    @conn = Faraday.new(url: "https://api.bestbuy.com/v1/products") do |faraday|
+  def initialize(search_params)
+    @conn = Faraday.new(url: "https://api.bestbuy.com/v1/products(longDescription=#{search_params}*)") do |faraday|
       faraday.request :url_encoded
       faraday.adapter Faraday.default_adapter
     end
   end
 
-  def search(search_url)
+  def search
     response = conn.get do |req|
-      req.url "#{search_url}"
       req.params['apiKey'] = ENV['BEST_BUY_KEY']
       req.params['format'] = "json"
       req.params['pageSize'] = '15'
       req.params['show'] = "sku,name,customerReviewAverage,shortDescription,salePrice,image"
-
     end
-    binding.pry
+    JSON.parse(response.body)
   end
 
 end
